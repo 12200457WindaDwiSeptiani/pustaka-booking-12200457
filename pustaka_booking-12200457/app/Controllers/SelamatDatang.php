802c313fd8e6b8c67905b1f29ad3f6e5c28a5274
12200457WindaDwiSeptiani <septianiwinda453@gmail.com>
2021-11-18 15:27:58 +0700

    Tugas Pertemuan 8

--- pustaka_booking-12200457/app/Controllers/SelamatDatang.php
+++ pustaka_booking-12200457/app/Controllers/SelamatDatang.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace App\Controllers;
 
-class SelamatDatang extends BaseController{
+use Config\Services;
+
+class SelamatDatang extends BaseController {
 
     public function hal_awal(){
         return 'hello saya belajar CI4';
@@ -13,7 +14,8 @@ class SelamatDatang extends BaseController{
             'vd' => $this->session->getFlashdata('validator'),
             'email' => $this->session->get('email'),
             'sandi' => $this->session->get('sandi'),
-            'error' => $this->session->getFlashdata('error')
+            'error' => $this->session->getFlashdata('error'),
+            'title' => 'Login sisfo Perpustakaan'
         ]);
     }
 
